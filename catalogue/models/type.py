7c9af06b15fb40ab7d11ca461af90ac95cbeb361
mohafor <mkechibil@gmail.com>
2023-05-09 20:22:28 +0200

    Update template Artist & Type

--- catalogue/models/type.py
+++ catalogue/models/type.py
@@ -1,8 +1,13 @@
 from django.db import models
+from .artist import *
 
 # Create your models here.
 class Type(models.Model):
 	type = models.CharField(max_length=60)
+	artists = models.ManyToManyField('catalogue.Artist', through='ArtistType')
+
+	def __str__(self):
+		return f"{self.type}"
 	
 	class Meta:
 		db_table = "types"
